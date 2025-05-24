17df9f493495282823224df1a77cb0fa967f2e3f
harikumar08 <163392850+harikumar08@users.noreply.github.com>
2025-05-24 08:38:23 +0530

    Time: 0 ms (100.00%) | Memory: 18.5 MB (77.89%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,43 +1,47 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        int m=matrix.size();
-        int n=matrix[0].size();
-        int first_row=-1;
-        int first_col=-1;
+        int n=matrix.size();
+        int m=matrix[0].size();
+        int first_row=1;
+        int first_col=1;
 
-        for(int i=0;i<m;i++){
-            if(matrix[i][0]==0) first_row=0;
+        for(int i=0;i<n;i++){
+            if(matrix[i][0]==0){
+                first_col=0;
+            }
         }
-        for(int j=0;j<n;j++){
-            if(matrix[0][j]==0) first_col=0;
+        for(int j=0;j<m;j++){
+            if(matrix[0][j]==0){
+                first_row=0;
+            }
         }
 
-
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
+        for(int i=1;i<n;i++){
+            for(int j=1;j<m;j++){
                 if(matrix[i][j]==0){
-                    matrix[i][0]=0;
                     matrix[0][j]=0;
+                    matrix[i][0]=0;
                 }
             }
         }
 
-
-        for(int i=1;i<m;i++){
-            for(int j=1;j<n;j++){
-                if(matrix[i][0]==0 || matrix[0][j]==0) matrix[i][j]=0;
+        for(int i=1;i<n;i++){
+            for(int j=1;j<m;j++){
+                if(matrix[0][j]==0 || matrix[i][0]==0){
+                    matrix[i][j]=0;
+                }
             }
         }
 
         if(first_row==0){
-            for(int i=0;i<m;i++){
-                matrix[i][0]=0;
+            for(int j=0;j<m;j++){
+                matrix[0][j]=0;
             }
         }
         if(first_col==0){
-            for(int j=0;j<n;j++){
-                matrix[0][j]=0;
+            for(int i=0;i<n;i++){
+                matrix[i][0]=0;
             }
         }
         return;
